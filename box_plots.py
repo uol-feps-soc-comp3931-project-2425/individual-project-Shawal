9ebe23d67bad56888dd02cafb3e44a343f12ec7a
Shawal19 <sc22sr2@leeds.ac.uk>
2025-02-18 13:46:28 +0000

    Box Plot

---
+++ box_plots.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#load your dataset
+df = pd.read_csv('eCommerce.csv')
+
+#DEBUGGING
+print(df.groupby('category')['CSAT Score'].describe())
+print(df.groupby('category')['CSAT Score'].var())
+
+#create a figure and specify the size
+plt.figure(figsize=(12,8))
+
+#create the box plot
+sns.boxplot(x='category', y='CSAT Score', data=df, palette='Set2')
+
+#customise the plot
+plt.title('CSAT Scores by Category', fontweight='bold')
+plt.xlabel('Category', fontstyle='italic')
+plt.ylabel('CSAT Score', fontstyle='italic')
+
+#rotate x-axis labels for better visibility
+plt.xticks(rotation=45, ha='right')
+
+#adjust layout and display
+plt.tight_layout()
+plt.show()
+
+
