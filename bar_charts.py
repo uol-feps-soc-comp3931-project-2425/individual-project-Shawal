8ff29453adcd46c3db7b76b7656c69e745a4f3c0
Shawal19 <sc22sr2@leeds.ac.uk>
2025-02-18 12:54:35 +0000

    Completed Bar Charts

--- bar_charts.py
+++ bar_charts.py
@@ -6,28 +6,28 @@ import matplotlib.pyplot as plt
 #load the dataset
 df = pd.read_csv('eCommerce.csv')
 
-# #group the data by 'category' and calculate the average CSAT score
-# avg_csat = df.groupby('category')['CSAT Score'].mean()
+#group the data by 'category' and calculate the average CSAT score
+avg_csat = df.groupby('category')['CSAT Score'].mean()
 
-# #create the bar chart
-# plt.figure(figsize=(10, 6))
-# avg_csat.plot(kind='bar', color='pink')
+#create the bar chart
+plt.figure(figsize=(10, 6))
+avg_csat.plot(kind='bar', color='pink')
 
-# #customise the chart
-# plt.xlabel('Category', fontstyle='italic')
-# plt.ylabel('Average CSAT Score', fontstyle='italic')
-# plt.title('Average CSAT Score by Category', fontweight='bold')
-# plt.xticks(rotation=45)
-# plt.tight_layout()     
+#customise the chart
+plt.xlabel('Category', fontstyle='italic')
+plt.ylabel('Average CSAT Score', fontstyle='italic')
+plt.title('Average CSAT Score by Category', fontweight='bold')
+plt.xticks(rotation=45)
+plt.tight_layout()     
 
-# #display the plot
-# plt.show()
+#display the plot
+plt.show()
 
 #SUBCATEGORY 
 
 import math
 
-selected_categories = ['Product Queries', 'Others']
+selected_categories = ['Order Related', 'Others']
 #'Order Related', 'Product Queries', 'Others', 'Cancellation'
 
 #filter the dataset to only include rows from the selected categories
@@ -58,6 +58,7 @@ for ax, category in zip(axes, unique_categories):
 plt.tight_layout()
 plt.show()
 
-# #print the result
-# print(avg_csat)
+#print the results
+print(avg_csat)
+print(subcat_csat)
 
