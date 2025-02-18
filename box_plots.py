1888a715bbf72fa0efcbcb58f7b839df09de81c5
Shawal19 <sc22sr2@leeds.ac.uk>
2025-02-18 14:14:21 +0000

    Added Violin Plot

--- box_plots.py
+++ box_plots.py
@@ -5,9 +5,9 @@ import seaborn as sns
 #load your dataset
 df = pd.read_csv('eCommerce.csv')
 
-#DEBUGGING
-print(df.groupby('category')['CSAT Score'].describe())
-print(df.groupby('category')['CSAT Score'].var())
+# #DEBUGGING
+# print(df.groupby('category')['CSAT Score'].describe())
+# print(df.groupby('category')['CSAT Score'].var())
 
 #create a figure and specify the size
 plt.figure(figsize=(12,8))
@@ -27,4 +27,25 @@ plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.show()
 
+#violin plot 
+
+print(df['CSAT Score'].describe())
+print(df['CSAT Score'].unique())  # Check all unique values
+print(df[df['CSAT Score'] > 5])   # Find all problematic rows
+
+#create a figure and specify the size
+plt.figure(figsize=(12,8))
+
+#create the violin plot
+sns.violinplot(x='category', y='CSAT Score', data=df, palette='Set2')
+
+#customise the plot
+plt.title('CSAT Scores by Category', fontweight='bold')
+plt.xlabel('Category', fontstyle='italic')
+plt.ylabel('CSAT Score', fontstyle='italic')
+
+#rotate x-axis labels for better visibility
+plt.xticks(rotation=45, ha='right')
+
+plt.show()
 
